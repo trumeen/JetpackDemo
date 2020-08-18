190580de5a3553e5d33f79a23fef275b4b6074d6
trumeen <hucm@galanz.com>
2020-08-18 18:27:11 +0800

    feat:加入启动页

---
+++ app/src/main/java/io/github/trumeen/ui/eyepetizer/SplashActivity.kt
@@ -0,0 +1,27 @@
+package io.github.trumeen.ui.eyepetizer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.os.Handler
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import io.github.trumeen.R
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+class SplashActivity : AppCompatActivity() {
+
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+}
